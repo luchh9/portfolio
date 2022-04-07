7aca93cc939d5fdecf163a580bc7f930b029a8a6
LucianoMariotti <luciano.mariotti99@gmail.com>
2022-04-07 00:48:45 -0300

    change navbar and planet-mail style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ export default function Navbar() {
             className="collapse navbar-collapse background-collapse"
             id="navbarNavAltMarkup"
           >
-            <div className="navbar-nav">
+            <div className="navbar-nav" style={{ margin: "0px 90px" }}>
               <a href="#about" className="nav-link">
                 About me
               </a>
